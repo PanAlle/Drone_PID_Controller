ada0888caa958267a8aa6e780b962ac7a03d7b04
PanAlle <217328@studenti.unimore.it>
2020-06-27 16:17:57 +0200

    COMMENT ADDED/ MATLAB FILE MOD
    I added some comments in the file.
    The matlab file now display overshoot and settling time in the case of step function given as input

--- Read_log_file.m
+++ Read_log_file.m
@@ -1,12 +1,29 @@
-file_name = 'Drone_PID_Controller.csv'
+file_name = 'Drone_PID_Controller.csv';
 
 %Start read the csv ar row 1, so avoid the inital name of fitrst row
 flight_data = csvread(file_name,1);
 
+%Check for overshoot only in the case of a step function
+if flight_data(2,5) == 1
+overshoot = find(flight_data(:,2) > 1, 1); 
+    % if overshoot vector is emty, then print no overshoot
+    if isempty(overshoot) == true
+        fprintf('No Overshoot \n');
+    end
+    %Compute settling time (+-5% to target)
+    fprintf('Settling time %i', flight_data(find(flight_data(:,2) >= 0.95, 1), 1));
+end
+
+% Plot the value to a 2d plot
 figure(1)
-plot(flight_data(:,1), flight_data(:,2), flight_data(:,1),flight_data(:,5))
+plot(flight_data(:,1), flight_data(:,2), flight_data(:,1),flight_data(:,5));
 
-
+% Title based on the simulation choosen
+if flight_data(2,5) == 1
+    title('System step response')
+else
+    title('System trajectory following')
+end
 %% try to define the system to develop the PID in matlab
 % -----------WIP---------------
 %data = iddata(flight_data(:,2),flight_data(:,5), 0.01);
--- main.c
+++ main.c
@@ -22,23 +22,24 @@ int main() {
     //Initialize error structure and pid parameters
     pid pid = {10,0,2};
     error err = { 0, 0, 0};
-    float pre_error = 0;
+    float pre_err = 0;
     //Start the simulation
     sim_start("C:\\Users\\utente\\CLionProjects\\Drone_PID_Controller\\Drone_PID_Controller.csv");
     float sim_time = 0;
-    //Simulate for 30 seconds
     printf("Press 1 for simulating a step function, press 2 to simulate a trajectory: \n");
     char selection = '0';
     scanf("%c", &selection);
+
+    //Simulate for 30 seconds
     while (sim_time <= 30){
 
+        //Select the different simulation.
         //Step function
         if (selection == '1'){
             sim_set_user_data(1);
             err.prp_error = 1 - sim_get_height();
         }
-
-        // Trajectory following case
+        // Trajectory following
         if (selection == '2'){
             sim_set_user_data(sim_get_example_trajectory(sim_time));
             err.prp_error = sim_get_example_trajectory(sim_time) - sim_get_height();
@@ -46,10 +47,10 @@ int main() {
 
 
 
-        err.der_error = (err.prp_error - pre_error)/TIME_STEP;
+        err.der_error = (err.prp_error - pre_err)/TIME_STEP;
         err.int_error += (err.prp_error * TIME_STEP);
 
-        pre_error = err.prp_error;
+        pre_err = err.prp_error;
         //Define new accelleration based on the pid parameters
         err.correction_val = pid.P  * err.prp_error + pid.D * err.der_error + pid.I * err.int_error;
         if (err.correction_val >= MAX_ACCELERATION) err.correction_val = MAX_ACCELERATION;
